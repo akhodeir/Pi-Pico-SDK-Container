6fde70aafe1bafdcbdd161c912fc648b84ff480e
Ahmed Moustafa <ahmed_moustafa@apple.com>
2024-09-25 11:15:54 +0200

    prepare for Pico2

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,1 +1,2 @@
+export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin/"
 docker build . -f Dockerfile -t pico-sdk:latest --progress=plain
--- Docker/run.sh
+++ Docker/run.sh
@@ -1,3 +1,4 @@
-docker run -d -it --name pico-sdk-cd --mount type=bind,source=${PWD},target=/home/dev pico-sdk:latest
+export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin/"
+docker run -d -it --name pico-sdk-cd --mount type=bind,source=${PWD}/work,target=/home/dev pico-sdk:latest
 
 docker exec -it pico-sdk-cd /bin/bash
